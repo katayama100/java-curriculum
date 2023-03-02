d105e524ba39a1e2a57967a051cfeacbeee7fef3
katayama10000 <yamasan7812@ezweb.ne.jp>
2023-03-02 10:00:59 +0900

    1.3 新規作成

---
+++ HelloWorld/src/sample/sample.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 1;
+		int b = 1;
+System.out.println("a" + "+" + "b" + "=" + (a + b));
+	}
+	
+
+}
